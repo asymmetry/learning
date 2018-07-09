357c99c7150ff0d6caffb917653f16f25dfda37a
Chao Gu <guchao.pku@gmail.com>
2018-07-09 10:53:36 -0400

    Add solution to problem 1

---
+++ 0001/solution_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        len_nums = len(nums)
+
+        for i in range(len_nums - 1):
+            for j in range(i + 1, len_nums):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+if __name__ == '__main__':
+    print(Solution().twoSum([2, 7, 11, 15], 9))
