f885299bab35d920be6357137786457e6afbffbc
Chao Gu <guchao.pku@gmail.com>
2018-07-09 10:37:06 -0400

    Add solution to problem 1

---
+++ 0001/solution_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        table = {}
+
+        for index, num in enumerate(nums):
+            if target - num in table:
+                return [table[target - num], index]
+            table[num] = index
+
+
+if __name__ == '__main__':
+    print(Solution().twoSum([2, 7, 11, 15], 9))
