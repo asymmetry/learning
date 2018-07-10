a7e3c5630225628c73cc56195e7b428b65b1d9b2
Chao Gu <guchao.pku@gmail.com>
2018-07-10 12:08:18 -0400

    Add solution to problem 5

---
+++ 0005/solution_1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# O(n^2)
+
+
+class Solution:
+
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        len_s = len(s)
+
+        p_list = []
+
+        for i in range(len_s):
+            p_list.append((i, i))
+
+        for i in range(len_s - 1):
+            if s[i] == s[i + 1]:
+                p_list.append((i, i + 1))
+
+        i = 0
+        while i < len(p_list):
+            if p_list[i][0] - 1 >= 0 and p_list[i][1] + 1 < len_s:
+                if s[p_list[i][0] - 1] == s[p_list[i][1] + 1]:
+                    p_list.append((p_list[i][0] - 1, p_list[i][1] + 1))
+            i += 1
+
+        return s[p_list[-1][0]:p_list[-1][1] + 1]
+
+
+if __name__ == '__main__':
+    print(Solution().longestPalindrome('babad'))
+    print(Solution().longestPalindrome('cbbd'))
---
+++ 0005/solution_2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# O(n^2)
+
+
+class Solution:
+
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        self.len_s = len(s)
+
+        start, end = 0, 0
+
+        for i in range(self.len_s):
+            len_sub_1 = self._expension(s, i, i)
+            len_sub_2 = self._expension(s, i, i + 1)
+
+            if len_sub_1 > len_sub_2:
+                if len_sub_1 > end - start:
+                    start = i - (len_sub_1 - 1) // 2
+                    end = i + (len_sub_1 - 1) // 2 + 1
+            else:
+                if len_sub_2 > end - start:
+                    start = i - (len_sub_2 - 2) // 2
+                    end = i + len_sub_2 // 2 + 1
+
+        return s[start:end]
+
+    def _expension(self, s, start, end):
+        while start >= 0 and end < self.len_s and s[start] == s[end]:
+            start -= 1
+            end += 1
+
+        return end - start - 1
+
+
+if __name__ == '__main__':
+    print(Solution().longestPalindrome('babad'))
+    print(Solution().longestPalindrome('cbbd'))
