cb321919275bd45f24f4b626fba57c8fba2a6a52
Chao Gu <guchao.pku@gmail.com>
2018-07-10 10:41:33 -0400

    Add solution to problem 3

---
+++ 0003/solution_1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# O(2n)
+
+
+class Solution:
+
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        len_s = len(s)
+
+        sub_s_set = set()
+        max_len = 0
+        start, end = 0, 0
+        while end < len_s:
+            if s[end] in sub_s_set:
+                len_sub_s = end - start
+                if len_sub_s > max_len:
+                    max_len = len_sub_s
+
+                sub_s_set.remove(s[start])
+                start = start + 1
+            else:
+                sub_s_set.add(s[end])
+                end = end + 1
+
+        if end - start > max_len:
+            max_len = end - start
+
+        return max_len
+
+
+if __name__ == '__main__':
+    print(Solution().lengthOfLongestSubstring('abcabcbb'))
+    print(Solution().lengthOfLongestSubstring('bbbbb'))
+    print(Solution().lengthOfLongestSubstring('pwwkew'))
---
+++ 0003/solution_2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# O(n)
+
+
+class Solution:
+
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        len_s = len(s)
+
+        sub_s_map = {}
+        max_len = 0
+        start = 0
+        for end in range(len_s):
+            if s[end] in sub_s_map:
+                start = sub_s_map[s[end]] + 1 if sub_s_map[s[end]] + 1 > start else start
+
+            len_sub_s = end + 1 - start
+            if len_sub_s > max_len:
+                max_len = len_sub_s
+
+            sub_s_map[s[end]] = end
+
+        return max_len
+
+
+if __name__ == '__main__':
+    print(Solution().lengthOfLongestSubstring('abcabcbb'))
+    print(Solution().lengthOfLongestSubstring('bbbbb'))
+    print(Solution().lengthOfLongestSubstring('pwwkew'))
