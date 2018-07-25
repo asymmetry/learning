eadfe016c6c53a79b0ecfe07689b927e1d527c8a
Chao Gu <guchao.pku@gmail.com>
2018-07-25 17:13:33 -0400

    Add solution to problem 6-10

--- 0010_regular_expression_matching/solution_2.py
+++ 0010_regular_expression_matching/solution_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # O(s*p)
-# TODO: dynamic programming
+# dynamic programming
 
 
 class Solution:
@@ -13,7 +13,24 @@ class Solution:
         :rtype: bool
         """
 
-        pass
+        if not p:
+            return not s
+
+        len_s = len(s)
+        len_p = len(p)
+
+        match = [[False] * (len_p + 1) for _ in range(len_s + 1)]
+
+        match[-1][-1] = True
+        for i in range(len_s, -1, -1):
+            for j in range(len_p - 1, -1, -1):
+                first_match = i < len_s and p[j] in (s[i], '.')
+                if j + 1 < len_p and p[j + 1] == '*':
+                    match[i][j] = match[i][j + 2] or (first_match and match[i + 1][j])
+                else:
+                    match[i][j] = first_match and match[i + 1][j + 1]
+
+        return match[0][0]
 
 
 if __name__ == '__main__':
